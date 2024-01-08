3aff1b7efeb82d5dcf29d3fa59077988e5e57234
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-01-08 18:00:11 -0300

    Initial config

---
+++ Restart-App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart-App
+//
+//  Created by Ivan Romero on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
